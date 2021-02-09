88f3ee9e68731e80f60e93f2829b793b8c01413f
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2021-02-09 23:54:11 +0000

    Arquivo xmonad.hs limpo

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
---  Arquio de configuração do Xmonad 
+--  Arquivo de configuração do Xmonad 
 --
 --      By Gabriel Grechuk.
 --
@@ -43,38 +43,20 @@ main = do
     }
 
 
-    `additionalKeys` -- Add some extra key bindings:
-    [     ((0, xK_Print), spawn "gnome-screenshot -i")
---      , ("M-S-q",   confirmPrompt myXPConfig "exit" (io exitSuccess))
---      , ("M-p",     shellPrompt myXPConfig)
---      , ("M-<Esc>", sendMessage (Toggle "Full"))
-    ]
+    `additionalKeys` 
+    [((0, xK_Print), spawn "gnome-screenshot -i")]
 
 
 
 myLayouts = noBorders tiled ||| noBorders (Mirror tiled) ||| noBorders Full ||| noBorders Circle ||| noBorders Grid
   where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
-     
-     -- The default number of windows in the master pane
+     tiled   = Tall nmaster delta ratio 
      nmaster = 1
-     
-     -- Default proportion of screen occupied by master pane
      ratio   = 1/2
-     
-     -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
 
 
---------------------------------------------------------------------------------
--- | Manipulate windows as they are created.  The list given to
--- @composeOne@ is processed from top to bottom.  The first matching
--- rule wins.
---
--- Use the `xprop' tool to get the info you need for these matches.
--- For className, use the second value that xprop gives you.
 myManageHook = composeAll
     [ className =? "Gimp"           --> doFloat
     , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat
@@ -82,17 +64,8 @@ myManageHook = composeAll
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore 
     , className =? "Steam"          --> doFloat
-    , className =? "steam"          --> doFullFloat -- bigpicture-mode
+    , className =? "steam"          --> doFullFloat 
     , className =? "MPlayer"        --> doFloat
     , (isFullscreen --> doFullFloat) 
     ]
 
---myManageHook = composeOne
---  [ className =? "Pidgin" -?> doFloat
---  , className =? "XCalc"  -?> doFloat
---  , className =? "mpv"    -?> doFloat
---  , isDialog              -?> doCenterFloat
-
-    -- Move transient windows to their parent:
---  , transience
---  ]
