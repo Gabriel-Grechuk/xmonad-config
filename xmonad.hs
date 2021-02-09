675c12be6ae49804f7b79379d68f392be90b2660
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2021-02-09 23:47:01 +0000

    Base da configuração

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,21 @@
---------------------------------------------------------------------------------
--- | Example.hs
+-------------------------------------------------------------------------------
 --
--- Example configuration file for xmonad using the latest recommended
--- features (e.g., 'desktopConfig').
+--  Arquio de configuração do Xmonad 
+--
+--      By Gabriel Grechuk.
+--
+-------------------------------------------------------------------------------
+
 module Main (main) where
 
---------------------------------------------------------------------------------
+
+
 import System.Exit
+
 import XMonad
+
 import XMonad.Config.Desktop
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
@@ -18,53 +25,32 @@ import XMonad.Layout.LayoutModifier
 import XMonad.Layout.Circle
 import XMonad.Layout.Grid
 import XMonad.Layout.DecorationMadness
-
---import XMonad.Layout.BinarySpacePartition (emptyBSP)
 import XMonad.Layout.NoBorders (noBorders, smartBorders)
 
---import XMonad.Layout.ResizableTile (ResizableTall(..))
---import XMonad.Layout.ToggleLayouts (ToggleLayout(..), toggleLayouts)
---import XMonad.Prompt
---import XMonad.Prompt.ConfirmPrompt
---import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig
 
 --------------------------------------------------------------------------------
 main = do
---  spawn "xmobar" -- Start a task bar such as xmobar.
-
-  -- Start xmonad using the main desktop configuration with a few
-  -- simple overrides:
   xmonad  $ ewmh desktopConfig
---  { modMask    = mod4Mask -- Use the "Win" key for the mod key
-    { modMask    = mod1Mask -- Use the "Alt" key for the mod key
-    , manageHook = myManageHook <+> manageHook desktopConfig
-    , layoutHook = smartBorders $ desktopLayoutModifiers $ myLayouts
-    , logHook    = dynamicLogString def >>= xmonadPropLog
-    , handleEventHook = handleEventHook def <+> fullscreenEventHook 
-    , terminal   =  "alacritty"
-    , normalBorderColor  = "gray" -- "#dddddd"
-    , focusedBorderColor = "rebeccapurple"  -- "#ff0000" don't use hex, not <24 bit safe
+    { modMask            = mod1Mask 
+    , manageHook         = myManageHook <+> manageHook desktopConfig
+    , layoutHook         = smartBorders $ desktopLayoutModifiers $ myLayouts
+    , logHook            = dynamicLogString def >>= xmonadPropLog
+    , handleEventHook    = handleEventHook def <+> fullscreenEventHook 
+    , terminal           = "alacritty"
+    , normalBorderColor  = "#dddddd" 
+    , focusedBorderColor = "#ff0000"  
     }
 
+
     `additionalKeys` -- Add some extra key bindings:
-    [     ((0, xK_Print), spawn "spectacle")
+    [     ((0, xK_Print), spawn "gnome-screenshot -i")
 --      , ("M-S-q",   confirmPrompt myXPConfig "exit" (io exitSuccess))
 --      , ("M-p",     shellPrompt myXPConfig)
 --      , ("M-<Esc>", sendMessage (Toggle "Full"))
     ]
 
---------------------------------------------------------------------------------
--- | Customize layouts.
---
--- This layout configuration uses two primary layouts, 'ResizableTall'
--- and 'BinarySpacePartition'.  You can also use the 'M-<Esc>' key
--- binding defined above to toggle between the current layout and a
--- full screen layout.
---
--- myLayouts = toggleLayouts (noBorders Full) others
---   where
---     others = ResizableTall 1 (1.5/100) (3/5) [] ||| emptyBSP
+
 
 myLayouts = noBorders tiled ||| noBorders (Mirror tiled) ||| noBorders Full ||| noBorders Circle ||| noBorders Grid
   where
@@ -79,16 +65,8 @@ myLayouts = noBorders tiled ||| noBorders (Mirror tiled) ||| noBorders Full ||| 
      
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
-     
---------------------------------------------------------------------------------
--- | Customize the way 'XMonad.Prompt' looks and behaves.  It's a
--- great replacement for dzen.
--- myXPConfig = def
---  { position          = Top
---  , alwaysHighlight   = True
---  , promptBorderWidth = 0
---  , font              = "xft:monospace:size=9"
---  }
+
+
 
 --------------------------------------------------------------------------------
 -- | Manipulate windows as they are created.  The list given to
